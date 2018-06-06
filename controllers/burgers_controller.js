eb07a4e439f83d745856efea45e88f13e1e9554e
steve27519 <stevenyurchuck@gmail.com>
2018-06-06 19:09:25 -0400

    initial commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// routes
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+    console.log("in put");
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+console.log('req.body', req.body);
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Double Cheese Burger",
+0
+),
+(
+"Big Boy Burger",
+0
+),
+(
+"Avacado Burger",
+0
+);
